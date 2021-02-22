e8f8ae99f9e050a0a3ae1c2d331ceee240126277
leibale <leibale1998@gmail.com>
2021-02-22 13:59:00 -0500

    use config_scheme instead of config.extra, add "try-for-free" boolean configuration, clean folder structure

--- mkdocs_modules_template/plugin.py
+++ mkdocs_modules_template/plugin.py
@@ -1,15 +1,22 @@
 from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
 from os import path
 from shutil import copy
 
 class ModulesTemplatePlugin(BasePlugin):
+    config_scheme = (
+        ('modules', config_options.Type(list, default=[])),
+        ('try-for-free', config_options.Type(bool, default=True))
+    )
+
     def on_config(self, config, **kwargs):
-        config['theme'].dirs.insert(0, path.abspath(path.join(path.dirname(__file__), '../overrides')))
-        config['extra_css'].append('extra.css') # will be copied into the site folder on post build
+        config['theme'].dirs.insert(0, path.abspath(path.join(path.dirname(__file__), './theme/overrides')))
+        config['extra_css'].append('mkdocs-modules-template.css') # will be copied into the site folder on post build
+        config['modules-template'] = self.config
         return config
 
     def on_pre_build(self, config):
         assert config['theme'].name == 'material'
 
     def on_post_build(self, config, **kwargs):
-        copy(path.abspath(path.join(path.dirname(__file__), '../extra.css')), path.join(config['site_dir'], 'extra.css'))
+        copy(path.abspath(path.join(path.dirname(__file__), './theme/style.css')), path.join(config['site_dir'], 'mkdocs-modules-template.css'))
